7c559479c11d2d98e817693bb2c3208dc38046fc
yanyuw <547274419@qq.com>
2019-09-13 22:46:18 +0800

    feat: add imgcontrol

---
+++ assets/scripts/begin.js
@@ -0,0 +1,28 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        startBtn:{
+            default: null,
+            type: cc.Button
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.startBtn.node.on('click', this.callback, this);
+        
+    },
+
+    start () {
+
+    },
+
+    callback: function(){
+        cc.director.loadScene("main");
+    }
+
+    // update (dt) {},
+});
--- assets/scripts/plot.js
+++ assets/scripts/plot.js
@@ -1,3 +1,11 @@
+//本脚本用于控制剧情流
+
+//控制图片切换，数组中依次为：左立绘，右立绘，背景图，其他图片
+var imgControl = {
+    3: ['smile'],
+    5: ['normal']
+}
+
 module.exports={
     story:[
         "hello，我是<color=#ff00ff>甄探</color>，是一名初出茅庐的见习侦探",
@@ -6,4 +14,5 @@ module.exports={
         "咦，这是什么？",
         "啊，是师父给我的信！",
     ],
+    imgControl: imgControl
 }
