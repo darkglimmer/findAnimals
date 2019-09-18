2b46ff413c025d58b43d1bb7e893c8b1dbfb4774
darkglimmer <2392897209@qq.com>
2019-09-18 14:48:47 +0800

    feat: add capture scenes

--- assets/scripts/star.js
+++ assets/scripts/star.js
@@ -2,26 +2,10 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // score: 2
     },
 
     onLoad () {
-        // for(var i = 0; i < this.score; i++){
-        //     var self = this;
-        //     cc.loader.loadRes('images/star.png', cc.SpriteFrame,function(err,spriteFrame){
-        //     //创建一个新的节点，因为cc.Sprite是组件不能直接挂载到节点上，只能添加到为节点的一个组件  
-        //         var newStar = new cc.Node('newStar')  
-        //         //调用新建的node的addComponent函数，会返回一个sprite的对象  
-        //         const sprite = newStar.addComponent(cc.Sprite)  
-        //         //给sprite的spriteFrame属性 赋值  
-        //         sprite.spriteFrame=spriteFrame
-        //         //把新的节点追加到self.node节点去。self.node，就是脚本挂载的节点  
-        //         self.node.addChild(newStar)  
-        //         newStar.setPosition(this.getNewStarPosition(i));
-        //     })
-        // }
-        // this.spawnNewStar();
-        // console.log(i)
+        
     },
 
 })
